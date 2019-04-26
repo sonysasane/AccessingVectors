e30f4ca43a11bcb385db733d0112202038a7e98f
sonysasane <45103490+sonysasane@users.noreply.github.com>
2019-04-25 22:59:43 -0500

    Add files via upload

---
+++ AccessingVectors.R
@@ -0,0 +1,28 @@
+
+
+
+#accessing elements of vector
+
+w <- c(1,3,4,9,8,0,6,23)
+w[1]
+w[6]
+w[1:6]
+w[4:7]
+
+#accessing all elements except 3rd one
+w[-3]
+
+w[0]
+w[9]
+
+#accessing random numbers in a vector
+v <- w[c(1,3,5)]
+y<- w[c(-1,-4,-6)]
+
+y
+w[c(1,3,5)]
+w[1,3,4]
+#accessing in reverse order
+w[6:2]
+
+
